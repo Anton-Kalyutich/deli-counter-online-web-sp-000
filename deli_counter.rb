d21a78fbb100ec31dbd76e98d4451e0bff0b37a8
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-22 04:19:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,9 @@
 katz_deli = []
 
 def line(katz_deli)
+  output = ""
   if katz_deli.length == 0
-    return "The line is currently empty"
+    "The line is currently empty"
   else
     puts ""
   end
