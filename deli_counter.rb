3a3ad798b48f8c25815bc4a3012e5771432e3599
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-22 04:26:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
   else
     output = "The line is currently: "
     katz_deli.each_with_index do |name, index|
-      output << "#{index+1}. "
+      output << "#{index+1}. #{name}"
   end
   output
 end
